ce96d747d2818bc529379d077ccdfd972fceb4e3
Knox Hutchinson <knox@knoxsdata.com>
2023-02-14 14:11:06 -0600

    SQS apps

--- dvac02/sqs/sender.py
+++ dvac02/sqs/sender.py
@@ -1,14 +1,18 @@
 import boto3
 import json
 
-sqs_client = boto3.resource('sqs')
-queue_name = 'dvac02'
+sqs_client = boto3.client('sqs')
+queue_name_in = 'firstqueue'
 
 
-def send_message(queue_name, message):
-    message = {"key": "value"}
+def send_message(queue_name: str, message: dict):
+    message = json.dumps(message)
+    queue_url = sqs_client.get_queue_url(QueueName=queue_name)['QueueUrl']
     response = sqs_client.send_message(
-        QueueUrl="https://us-west-2.queue.amazonaws.com/xxx/my-new-queue",
-        MessageBody=json.dumps(message)
+        QueueUrl=queue_url,
+        MessageBody=message
     )
     print(response)
+
+
+send_message(queue_name_in, {'message': 'Hello World'})
