b6ce7a8c6d38889d1657c9f68473998852fbef4c
Knox Hutchinson <knox@knoxsdata.com>
2023-02-16 14:29:57 -0600

    staging ecs

---
+++ dvac02/ecs/bot.py
@@ -0,0 +1,17 @@
+# bot.py
+import os
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+client.run(TOKEN)
