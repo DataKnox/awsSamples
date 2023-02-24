82b4c164670b23fe425eb82b9477ca513deb7d26
Knox Hutchinson <knox@knoxsdata.com>
2023-02-24 09:24:16 -0600

    cleanup producer

--- dvac02/ecs_sqs_compose/producer.py
+++ dvac02/ecs_sqs_compose/producer.py
@@ -1,19 +1,25 @@
 import boto3
 import json
-import os, time
+import os
+import time
+from random import randint
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 sqs_client = boto3.client('sqs')
 queue_name_in = os.getenv('SQS_QUEUE_URL')
 
 
 def send_message(queue_url: str, message: dict):
-    while True:
-        message = json.dumps(message)
-        response = sqs_client.send_message(
-            QueueUrl=queue_url,
-            MessageBody=message
-        )
-        print(response)
-        time.sleep(35)
+    message = json.dumps(message)
+    response = sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message
+    )
+    logger.info(response)
 
 
-send_message(queue_name_in, {'message': 'Hello World'})
+while True:
+    num = randint(1, 1000)
+    send_message(queue_name_in, {'message': f'Hello World {str(num)}'})
+    time.sleep(35)
