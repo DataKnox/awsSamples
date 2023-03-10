e884a9b1ac35faad05bc2dbb7111d761bce0e228
Knox Hutchinson <knox@knoxsdata.com>
2023-03-10 14:54:58 -0600

    names

--- dvac02/elasticache/ec2_setup.sh
+++ dvac02/elasticache/ec2_setup.sh
@@ -1,8 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3-pip -y
- pip3 install boto3
- pip3 install redis
- pip3 install redis-py-cluster
+pip3 install boto3
+pip3 install redis
+pip3 install names
+# pip3 install redis-py-cluster
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
