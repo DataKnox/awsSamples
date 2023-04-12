26cbdca2acb7a8564d44085dab1e93a536e71f60
Knox Hutchinson <knox@knoxsdata.com>
2023-04-12 09:49:30 -0500

    firehose logging

---
+++ dvac02/kinesis_s3/loghose.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+from logging.handlers import TimedRotatingFileHandler
+
+
+def create_timed_rotating_log(path):
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+
+    handler = TimedRotatingFileHandler(path,
+                                       when="s",
+                                       interval=10,
+                                       backupCount=5)
+    logger.addHandler(handler)
+
+    for i in range(6):
+        logger.info({"message": "This is a test message", "count": i})
+        time.sleep(15)
+
+
+if __name__ == "__main__":
+    log_file = "loghose.log"
+    create_timed_rotating_log(log_file)
+
+
+# SOURCE: https://docs.python.org/2/howto/logging-cookbook.html#using-file-rotation
